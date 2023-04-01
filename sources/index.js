576e9a2f31d48d0f6276f6e47fc4a516a52a277d
imperatorm <32312789+imperatorm@users.noreply.github.com>
2023-04-01 19:49:38 +0200

    grain

--- sources/index.js
+++ sources/index.js
@@ -4,3 +4,14 @@ import "./styles/fonts.scss";
 
 const canvas = document.querySelector(".experience");
 const app = new App({ canvas });
+
+var options = {
+    "animate": true,
+    "patternWidth": 600,
+    "patternHeight": 600,
+    "grainOpacity": 0.15,
+    "grainDensity": 1,
+    "grainWidth": 0.7,
+    "grainHeight": 0.7,
+  }
+  grained("#grain", options);
